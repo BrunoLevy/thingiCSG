446e9c25108017aafae20c6e9c21656e5a0d867d
Bruno <Bruno.Levy@inria.fr>
2025-02-23 07:19:43 +0100

    Reorg BOSL cases Script to launch all test cases

---
+++ scripts/compute_all.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+sources=`ls *.scad`
+engine="geogram"
+
+while [ -n "$1" ]; do
+
+    case "$1" in
+       -engine)
+          shift
+	  engine=$1
+	  shift
+	  ;;
+
+       -i)
+          shift
+	  sources=$1
+	  shift
+	  ;;
+
+        *)
+          echo "Error: unrecognized option: $1"
+          return 1
+          ;;
+
+    esac
+done    
+
+case "$engine" in
+   geogram)
+      command="compute_CSG"
+      flags=""
+      extension=".geogram"
+      args=""
+      ;;
+      
+   CGAL)
+      command="openscad"
+      flags=""
+      extension=".stl"
+      args="-o"
+      ;;
+      
+   coref*)
+      engine="corefinement"
+      command="openscad++"
+      flags="--enable fast-csg-trust-corefinement"
+      extension=".stl"
+      args="-o"
+      ;;
+
+   manifold)
+      command="openscad++"
+      flags="--enable manifold"
+      extension=".stl"
+      args="-o"
+      ;;
+
+   *)
+      echo "Error: unrecognized engine: $engine"
+      return 1
+      ;;
+esac
+
+echo "input  :" $sources
+echo "engine :" $engine
+
+for input in $sources
+do
+   echo
+   echo "Processing >>>" $input "<<<"
+   $command $flags $input $args `basename $input .scad`$extension
+done
